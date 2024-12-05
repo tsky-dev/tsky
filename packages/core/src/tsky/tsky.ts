6100c169d28e0693b8ffbec22bdb96c83a38e84c
Anbraten <anton@ju60.de>
2024-12-05 09:06:25 +0100

    init

--- packages/core/src/tsky/tsky.ts
+++ packages/core/src/tsky/tsky.ts
@@ -1,24 +1,19 @@
-import type {
-  AppBskyActorSearchActors,
-  AppBskyActorSearchActorsTypeahead,
-} from '@atproto/api';
+import { type FetchHandler, XRPC } from '@atcute/client';
 import { Feed } from '~/bsky';
 import { Paginator } from './paginator';
-import type { Session } from './session';
-import { XrpcClient } from './xrpc';
 
 export class TSky {
-  xrpc: XrpcClient;
+  xrpc: XRPC;
 
-  constructor(session: Session) {
-    this.xrpc = new XrpcClient(session);
+  constructor(handler: FetchHandler) {
+    this.xrpc = new XRPC({ handler });
   }
 
   /**
    * Get detailed profile view of an actor. Does not require auth, but contains relevant metadata with auth.
    */
-  async profile(identifier: string | string[]) {
-    const res = await this.xrpc.request('app.bsky.actor.getProfile', 'GET', {
+  async profile(identifier: string) {
+    const res = await this.xrpc.get('app.bsky.actor.getProfile', {
       actor: identifier,
     });
 
@@ -27,40 +22,5 @@ export class TSky {
 
   get feed() {
     return new Feed(this.xrpc);
-  }
-
-  /**
-   * Find actor suggestions for a prefix search term. Expected use is for auto-completion during text field entry. Does not require auth.
-   */
-  async typeahead(
-    params: AppBskyActorSearchActorsTypeahead.QueryParams,
-    options?: AppBskyActorSearchActorsTypeahead.CallOptions,
-  ) {
-    const res = await this.instance.actor.searchActorsTypeahead(
-      params,
-      options,
-    );
-
-    return res.data.actors;
-  }
-
-  /**
-   * Find actors (profiles) matching search criteria. Does not require auth.
-   */
-  async search(
-    params: AppBskyActorSearchActors.QueryParams = {},
-    options?: AppBskyActorSearchActors.CallOptions,
-  ) {
-    return new Paginator(async (cursor) => {
-      const res = await this.instance.actor.searchActors(
-        {
-          cursor,
-          ...params,
-        },
-        options,
-      );
-
-      return res.data;
-    });
   }
 }
