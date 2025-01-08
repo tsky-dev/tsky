28f9a1e759718f1a191325248cf06da0bc8a7653
TAKAHASHI Shuuji <shuuji3@gmail.com>
2025-01-08 23:35:07 +0900

    chore: remove debug log

--- packages/client/src/bsky/index.test.ts
+++ packages/client/src/bsky/index.test.ts
@@ -24,8 +24,6 @@ const TEST_CREDENTIALS = {
   },
 };
 
-console.log('env keys', Object.keys(process.env));
-
 async function getAliceTsky() {
   const manager = new CredentialManager({ service: 'https://bsky.social' });
   await manager.login({
