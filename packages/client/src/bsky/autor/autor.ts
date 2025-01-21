bc5b04317038560b9b9ce9ace2bb10cd14acb791
Aditya Mathur <mathuraditya724@gmail.com>
2025-01-21 18:48:32 +0530

    fix(client): added more props in the actor class

--- packages/client/src/bsky/autor/autor.ts
+++ packages/client/src/bsky/autor/autor.ts
@@ -1,4 +1,7 @@
-import type { AppBskyActorDefs } from '@tsky/lexicons';
+import type {
+  AppBskyActorDefs,
+  AppBskyFeedGetAuthorFeed,
+} from '@tsky/lexicons';
 import type { Client } from '~/tsky/client';
 import type { RPCOptions } from '~/types';
 import { Paginator } from '~/utils';
@@ -84,6 +87,52 @@ export class Actor {
           actor: this.identifier,
           limit,
         },
+        ...options,
+      });
+
+      return res.data;
+    });
+  }
+
+  /**
+   * Enumerates public relationships between one account, and a list of other accounts. Does not require auth.
+   */
+  async relationships(others?: string[], options?: RPCOptions) {
+    const res = await this.client.get('app.bsky.graph.getRelationships', {
+      params: {
+        actor: this.identifier,
+        others,
+      },
+      ...options,
+    });
+
+    return res.data;
+  }
+
+  /**
+   * Get a view of an actor's 'author feed' (post and reposts by the author). Does not require auth.
+   */
+  feeds(limit?: number, options?: RPCOptions) {
+    return Paginator.init(async (cursor) => {
+      const res = await this.client.get('app.bsky.feed.getActorFeeds', {
+        params: { cursor, actor: this.identifier, limit },
+        ...options,
+      });
+
+      return res.data;
+    });
+  }
+
+  /**
+   * Get a list of feeds (feed generator records) created by the actor (in the actor's repo).
+   */
+  feed(
+    params?: Omit<AppBskyFeedGetAuthorFeed.Params, 'actor'>,
+    options?: RPCOptions,
+  ) {
+    return Paginator.init(async (cursor) => {
+      const res = await this.client.get('app.bsky.feed.getAuthorFeed', {
+        params: { cursor, ...params, actor: this.identifier },
         ...options,
       });
 
