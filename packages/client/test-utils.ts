3e363470d4c74c5a0467e213927bca0cc692e67c
TAKAHASHI Shuuji <shuuji3@gmail.com>
2025-02-13 01:00:16 +0900

    test(test-utils): prepare agent instances for alice and bob beforehand

--- packages/client/test-utils.ts
+++ packages/client/test-utils.ts
@@ -1,19 +1,30 @@
 import { inject } from 'vitest';
+
+import type { Agent } from '~/agent';
 import { createAgent } from '~/tsky';
 
-/**
- * Get Agent instance for testing accounts.
- * There are `@alice.test` and `@bob.test` now.
- * @param handle - handle name for test agent (without `@`)
- */
-export async function getTestAgent(handle: 'alice.test' | 'bob.test') {
-  const agent = await createAgent(
+type Handle = 'alice.test' | 'bob.test';
+
+const testAgents: Record<Handle, Agent> = {
+  'alice.test': await createTestAgent('alice.test'),
+  'bob.test': await createTestAgent('bob.test'),
+};
+
+function createTestAgent(handle: Handle) {
+  return createAgent(
     {
       identifier: handle,
       password: 'password',
     },
     { service: inject('testPdsUrl') },
   );
+}
 
-  return agent;
+/**
+ * Get Agent instance for testing accounts.
+ * There are `@alice.test` and `@bob.test` now.
+ * @param handle - handle name for test agent (without `@`)
+ */
+export async function getTestAgent(handle: Handle) {
+  return testAgents[handle];
 }
