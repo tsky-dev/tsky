dbb8c7350492bf87f423e6794c4e5766295fbbf8
TAKAHASHI Shuuji <shuuji3@gmail.com>
2026-01-16 23:34:42 +0900

    chore: remove lex-cli and lexicons initially bundled from atcute

--- packages/lex-cli/src/generator/resolvers/index.ts
+++
@@ -1,9 +0,0 @@
-export {
-  resolveObjectType,
-  resolveRefType,
-  resolveUnionType,
-} from './complex.js';
-export { resolveIntegerType } from './numeric.js';
-export { resolvePrimitiveType } from './primitives.js';
-export { resolveStringFormat, resolveStringType } from './string.js';
-export { resolveType } from './type.js';
